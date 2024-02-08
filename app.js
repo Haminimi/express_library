9e6bbaa06674a99695485f1aef891f91cb111619
Hamza Mujinovic <haminimi.dev@gmail.com>
2024-02-08 19:02:39 +0100

    Secure the connection string and add the compression middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require('compression');
 
 const indexRouter = require('./routes/index');
 const catalogRouter = require('./routes/catalog');
@@ -11,8 +12,9 @@ const app = express();
 
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
-const mongoDB = require('./connection-string');
+const connectionString = require('./connection-string');
 
+const mongoDB = process.env.MONGODB_URI || connectionString;
 main().catch((err) => console.log(err));
 async function main() {
 	await mongoose.connect(mongoDB);
@@ -22,6 +24,7 @@ async function main() {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(compression());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
